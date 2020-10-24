04ba767f3101ef8bebb5b250cba247ac6a76ea12
WilliWont <tiennh.will@gmail.com>
2020-10-24 15:52:49 +0700

    traningCICD change

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
 * @param {Object} res Cloud Function response context.
 * More info: https://expressjs.com/en/api.html#res
 */
-exports.trainingCICD = (req, res) => {
-  res.send(`Hello ${req.body.name || 'World'}!`);
+exports.traningCICD = (req, res) => {
+  res.send({ yourName: req.body.name });
 };
